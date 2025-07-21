4a6eb34acdc6d7577b64a3794d6c2b62fb921397
D3K1D <120131611+D3K1D@users.noreply.github.com>
2025-07-20 20:14:05 -0400

    Update settings.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
